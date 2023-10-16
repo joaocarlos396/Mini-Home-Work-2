6655b53b48673b3e9224967a752ad547d85c3502
joaocarlos396 <2022172@student.cct.ie>
2023-10-16 20:54:29 +0100

    firts
    first commit

--- src/oocminihw2/OOCMiniHW2.java
+++ src/oocminihw2/OOCMiniHW2.java
@@ -14,7 +14,6 @@ public class OOCMiniHW2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //test
         Car car = new Car("Toyota", "Sedan", 4);
         Airplane airplane = new Airplane("Boeing", "Jet", 150);
         Boat Boat = new Boat("Beneteau", "Yacht", 8);
